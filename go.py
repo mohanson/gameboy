a933e73b1c1157bf4b6851d99d8db6e2f006d65d
mohanson <mohanson@outlook.com>
2019-04-23 22:08:02 +0800

    2019-04-23 22:08:01

--- go.py
+++ go.py
@@ -1,3 +1,4 @@
+import os.path
 import subprocess
 import sys
 
@@ -13,36 +14,41 @@ def make():
     call('cargo build')
 
 
-path_rom_only = r"/tmp/gb/3D Wireframe Demo (PD) [C].gbc"
-path_mbc1 = r"/tmp/gb/175 Sprite Parallax Starfield Demo (PD) [C].gb"
-path_mbc1_ram = r"/tmp/gb/AGO Realtime Demo (LCP2000) (PD) [C].gbc"
-path_mbc1_ram_battery = r"/tmp/gb/Boxes (PD).gb"
-path_mbc2_battery = r"/tmp/gb/Fastest Lap (JU) [b1].gb"
-path_mbc3_ram_battery = r"/tmp/gb/pokemon_blud.gb"
-path_mbc3_timer_ram_battery = r"/tmp/gb/Pokemon - Crystal Version (US).gbc"
-path_mbc5_ram_battery = r"/tmp/gb/Alice in Wonderland (U) [C][!].gbc"
-path_cpu_instrs = r"/tmp/gb/cpu_instrs.gb"
-path_cpu_instr_timing = r"/tmp/gb/instr_timing.gb"
-
-
 def test():
-    make()
-    for p in [
-        path_rom_only,
-        path_mbc1,
-        path_mbc1_ram,
-        path_mbc1_ram_battery,
-        path_mbc2_battery,
-        path_mbc3_ram_battery,
-        path_mbc3_timer_ram_battery,
-        path_mbc5_ram_battery
-    ]:
-        call(f'target\\debug\\gameboy.exe "{p}"')
+    if not os.path.exists('/tmp/gb-test-roms'):
+        call('git clone --depth=1 https://github.com/retrio/gb-test-roms /tmp/gb-test-roms')
+    call(f'cargo run -- /tmp/gb-test-roms/cpu_instrs/cpu_instrs.gb')
+
+
+# path_rom_only = r"/tmp/gb/3D Wireframe Demo (PD) [C].gbc"
+# path_mbc1 = r"/tmp/gb/175 Sprite Parallax Starfield Demo (PD) [C].gb"
+# path_mbc1_ram = r"/tmp/gb/AGO Realtime Demo (LCP2000) (PD) [C].gbc"
+# path_mbc1_ram_battery = r"/tmp/gb/Boxes (PD).gb"
+# path_mbc2_battery = r"/tmp/gb/Fastest Lap (JU) [b1].gb"
+# path_mbc3_ram_battery = r"/tmp/gb/pokemon_blud.gb"
+# path_mbc3_timer_ram_battery = r"/tmp/gb/Pokemon - Crystal Version (US).gbc"
+# path_mbc5_ram_battery = r"/tmp/gb/Alice in Wonderland (U) [C][!].gbc"
+# path_cpu_instrs = r"/tmp/gb/cpu_instrs.gb"
+# path_cpu_instr_timing = r"/tmp/gb/instr_timing.gb"
+
+
+# def test():
+#     make()
+#     for p in [
+#         path_rom_only,
+#         path_mbc1,
+#         path_mbc1_ram,
+#         path_mbc1_ram_battery,
+#         path_mbc2_battery,
+#         path_mbc3_ram_battery,
+#         path_mbc3_timer_ram_battery,
+#         path_mbc5_ram_battery
+#     ]:
+#         call(f'target\\debug\\gameboy.exe "{p}"')
 
 
 def main():
     make()
-    call(f'target\\debug\\gameboy.exe -a "{path_mbc1_ram_battery}"')
 
 
 if __name__ == '__main__':
