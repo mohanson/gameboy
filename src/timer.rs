3d35b0a13449f3f66e82203b6172629a2e6792f1
Mohanson <mohanson@outlook.com>
2019-04-26 20:50:04 +0800

    Rewrite the timer

--- src/timer.rs
+++ src/timer.rs
@@ -14,23 +14,25 @@ pub struct Timer {
     // This register is incremented at rate of 16384Hz (~16779Hz on SGB). Writing any value to this register resets it
     // to 00h.
     // Note: The divider is affected by CGB double speed mode, and will increment at 32768Hz in double speed.
-    ff04: u8,
+    div: u8,
     // This timer is incremented by a clock frequency specified by the TAC register ($FF07). When the value overflows
     // (gets bigger than FFh) then it will be reset to the value specified in TMA (FF06), and an interrupt will be
     // requested, as described below.
-    ff05: u8,
+    tima: u8,
     // When the TIMA overflows, this data will be loaded.
-    ff06: u8,
+    tma: u8,
     //  Bit  2   - Timer Enable
     //  Bits 1-0 - Input Clock Select
     //             00: CPU Clock / 1024 (DMG, CGB:   4096 Hz, SGB:   ~4194 Hz)
     //             01: CPU Clock / 16   (DMG, CGB: 262144 Hz, SGB: ~268400 Hz)
     //             10: CPU Clock / 64   (DMG, CGB:  65536 Hz, SGB:  ~67110 Hz)
     //             11: CPU Clock / 256  (DMG, CGB:  16384 Hz, SGB:  ~16780 Hz)
-    ff07: u8,
+    tac: u8,
 
     freq: usize,
+    // Count the number of cycles and set 0 each 256 cycles
     tmp1: usize,
+    // Count the number of cycles and set 0 each "freq" cycles
     tmp2: usize,
 }
 
@@ -44,21 +46,21 @@ impl Timer {
 
     pub fn get(&self, a: u16) -> u8 {
         match a {
-            0xff04 => self.ff04,
-            0xff05 => self.ff05,
-            0xff06 => self.ff06,
-            0xff07 => self.ff07,
+            0xff04 => self.div,
+            0xff05 => self.tima,
+            0xff06 => self.tma,
+            0xff07 => self.tac,
             _ => panic!("Unsupported address"),
         }
     }
 
     pub fn set(&mut self, a: u16, v: u8) {
         match a {
-            0xff04 => self.ff04 = 0x00,
-            0xff05 => self.ff05 = v,
-            0xff06 => self.ff06 = v,
+            0xff04 => self.div = 0x00,
+            0xff05 => self.tima = v,
+            0xff06 => self.tma = v,
             0xff07 => {
-                self.ff07 = v;
+                self.tac = v;
                 match v & 0x03 {
                     0x00 => self.freq = 1024,
                     0x01 => self.freq = 16,
@@ -72,18 +74,21 @@ impl Timer {
     }
 
     pub fn next(&mut self, cycles: usize) {
+        // Increment div at rate of 16384Hz. Because the clock cycles is 4194304, so div increment every 256 cycles.
         let c = 256;
         self.tmp1 += cycles;
         if self.tmp1 >= c {
-            self.ff04 = self.ff04.wrapping_add(1);
+            self.div = self.div.wrapping_add(1);
             self.tmp1 -= c;
         }
-        if (self.ff07 & 0x04) != 0x00 {
+
+        // Increment tima at rate of Clock / freq
+        if (self.tac & 0x04) != 0x00 {
             self.tmp2 += cycles;
             while self.tmp2 >= self.freq {
-                self.ff05 = self.ff05.wrapping_add(1);
-                if self.ff05 == 0x00 {
-                    self.ff05 = self.ff06;
+                self.tima = self.tima.wrapping_add(1);
+                if self.tima == 0x00 {
+                    self.tima = self.tma;
                     self.interrupt |= 0x04;
                 }
                 self.tmp2 -= self.freq;
