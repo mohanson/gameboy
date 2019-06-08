5bdffdfe235fd99ee98d14dd89bd165b4a0f2461
mohanson <mohanson@outlook.com>
2019-06-08 13:25:13 +0800

    2019-06-08 13:25:13

--- src/gpu.rs
+++ src/gpu.rs
@@ -608,10 +608,40 @@ impl Gpu {
         }
     }
 
+    // Gameboy video controller can display up to 40 sprites either in 8x8 or in 8x16 pixels. Because of a limitation
+    // of hardware, only ten sprites can be displayed per scan line. Sprite patterns have the same format as BG tiles,
+    // but they are taken from the Sprite Pattern Table located at $8000-8FFF and have unsigned numbering.
+    //
+    // Sprite attributes reside in the Sprite Attribute Table (OAM - Object Attribute Memory) at $FE00-FE9F. Each of
+    // the 40 entries consists of four bytes with the following meanings:
+    //   Byte0 - Y Position
+    //   Specifies the sprites vertical position on the screen (minus 16). An off-screen value (for example, Y=0 or
+    //   Y>=160) hides the sprite.
+    //
+    //   Byte1 - X Position
+    //   Specifies the sprites horizontal position on the screen (minus 8). An off-screen value (X=0 or X>=168) hides
+    //   the sprite, but the sprite still affects the priority ordering - a better way to hide a sprite is to set its
+    //   Y-coordinate off-screen.
+    //
+    //   Byte2 - Tile/Pattern Number
+    //   Specifies the sprites Tile Number (00-FF). This (unsigned) value selects a tile from memory at 8000h-8FFFh. In
+    //   CGB Mode this could be either in VRAM Bank 0 or 1, depending on Bit 3 of the following byte. In 8x16 mode, the
+    //   lower bit of the tile number is ignored. IE: the upper 8x8 tile is "NN AND FEh", and the lower 8x8 tile is
+    //   "NN OR 01h".
+    //
+    //   Byte3 - Attributes/Flags:
+    //     Bit7   OBJ-to-BG Priority (0=OBJ Above BG, 1=OBJ Behind BG color 1-3)
+    //           (Used for both BG and Window. BG color 0 is always behind OBJ)
+    //     Bit6   Y flip          (0=Normal, 1=Vertically mirrored)
+    //     Bit5   X flip          (0=Normal, 1=Horizontally mirrored)
+    //     Bit4   Palette number  **Non CGB Mode Only** (0=OBP0, 1=OBP1)
+    //     Bit3   Tile VRAM-Bank  **CGB Mode Only**     (0=Bank 0, 1=Bank 1)
+    //     Bit2-0 Palette number  **CGB Mode Only**     (OBP0-7)
     fn draw_sprites(&mut self) {
+        // Sprite tile size 8x8 or 8x16(2 stacked vertically).
         let sprite_size = if self.lcdc.bit2() { 16 } else { 8 };
         for i in 0..40 {
-            let sprite_addr = 0xFE00 + (i as u16) * 4;
+            let sprite_addr = 0xfe00 + (i as u16) * 4;
             let sprite_y = i32::from(self.get(sprite_addr)) - 16;
             let sprite_x = i32::from(self.get(sprite_addr + 1)) - 8;
             let tile_location = u16::from(self.get(sprite_addr + 2)) & (if self.lcdc.bit2() { 0xfe } else { 0xff });
